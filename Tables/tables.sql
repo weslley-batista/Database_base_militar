516db91cd8b7c0fc4783eac50d82e098010aee08
weslley batista <wbh@cin.ufpe.br>
2022-03-22 14:54:36 -0300

    tabelas relacionadas a entidade PESSOA

--- Tables/tables.sql
+++ Tables/tables.sql
@@ -1,5 +1,19 @@
-Create Table pessoa (
+Create Table pessoa ( --entidade
     nome varchar2,
     cpf number (11),
-    pessoa_pkey primary key(cpf)
+    constraint pessoa_pkey primary key(cpf)
+);
+
+Create Table endereco_da_pessoa ( --composto
+    cpf_pessoa number(11),
+    rua varchar2,
+    numero number,
+    complemento varchar2,
+    cep number,
+    constraint endereco_da_pessoa_fkey Foreign key (cpf_pessoa) References pessoa (cpf)
+);
+Create Table telefone_da_pessoa ( --multivalorado
+    cpf_pessoa number(11),
+    numero_telefone number,
+    constraint telefone_da_pessoa_fkey Foreign key (cpf_pessoa) References pessoa (cpf)
 );
