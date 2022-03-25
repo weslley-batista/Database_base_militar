fa153fd6e3b9242428b985ade4aa6b8544a03e03
PedrovlBarbosa <55454762+PedrovlBarbosa@users.noreply.github.com>
2022-03-25 15:45:55 -0300

    Update table_pedro.sql

--- Tables/table_pedro.sql
+++ Tables/table_pedro.sql
@@ -14,18 +14,18 @@ Create Table arma ( -- entidade
     calibre varchar (255) NOT NULL, -- .556
     fabricação varchar (255) NOT NULL,
     nome_do_armamento varchar (255) NOT NULL,
-    horario_e_data datetime (fsp) NOT NULL,
+    horario_e_data datetime NOT NULL,
     constraint numero_de_registro_pkey primary key (numero_de_registro), -- verificar
     constraint id_arsenal_fkey foreign key (id_arsenal) references arsenal (id_pkey),
     constraint horario_e_data_fkey foreign key (horario_e_data) references fornecer_armamento (horario_e_data) -- verificar
 );
 
 Create Table fornecer_armamento ( -- relacionamento triplo temporal
-    horario_e_data datetime2(fsp) NOT NULL,
+    horario_e_data datetime NOT NULL,
     cpf_militar varchar(14) NOT NULL, -- 000.000.000-00
     registro_arma varchar (255) NOT NULL,
     id_arsenal int NOT NULL,
     constraint cpf_militar_fkey foreign key (cpf_militar) references militar (cpf),
     constraint registro_arma_fkey foreign key (registro_arma) references arma (numero_de_registro_pkey), -- verificar
-    constraint id_arsenal_fkey foreign key (id_arsenal) references arsenal (id_pkey),
+    constraint id_arsenal_fkey foreign key (id_arsenal) references arsenal (id_pkey)
 );
