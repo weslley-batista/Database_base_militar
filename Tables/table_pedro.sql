21f418e929388d32be2be481fb02e75354f08693
PedrovlBarbosa <55454762+PedrovlBarbosa@users.noreply.github.com>
2022-03-25 16:35:30 -0300

    Update table_pedro.sql

--- Tables/table_pedro.sql
+++ Tables/table_pedro.sql
@@ -1,9 +1,9 @@
 Create Table arsenal ( -- entidade
     Create Sequence id increment by 1 start with 1,
-    categoria_armazem varchar (255) NOT NULL,
+    categoria_armazem varchar2 (255) NOT NULL,
     data_de_manutenção date NOT NULL,
     capacidade_máxima int NOT NULL,
-    nome_base_militar varchar (255) NOT NULL,
+    nome_base_militar varchar2 (255) NOT NULL,
     constraint id_pkey primary key (id),
     constraint nome_base_militar_fkey foreign key (nome_base_militar) references base_militar (nome)
 );
@@ -11,9 +11,9 @@ Create Table arsenal ( -- entidade
 Create Table arma ( -- entidade
     numero_de_registro number (255) NOT NULL,
     id_arsenal int NOT NULL, -- 
-    calibre varchar (255) NOT NULL, -- .556
-    fabricação varchar (255) NOT NULL,
-    nome_do_armamento varchar (255) NOT NULL,
+    calibre varchar2 (255) NOT NULL, -- .556
+    fabricação varchar2 (255) NOT NULL,
+    nome_do_armamento varchar2 (255) NOT NULL,
     horario_e_data datetime NOT NULL,
     constraint numero_de_registro_pkey primary key (numero_de_registro), -- verificar
     constraint id_arsenal_fkey foreign key (id_arsenal) references arsenal (id_pkey),
@@ -22,8 +22,8 @@ Create Table arma ( -- entidade
 
 Create Table fornecer_armamento ( -- relacionamento triplo temporal
     horario_e_data datetime NOT NULL,
-    cpf_militar varchar(14) NOT NULL, -- 000.000.000-00
-    registro_arma varchar (255) NOT NULL,
+    cpf_militar varchar2 (14) NOT NULL, -- 000.000.000-00
+    registro_arma varchar2 (255) NOT NULL,
     id_arsenal int NOT NULL,
     constraint cpf_militar_fkey foreign key (cpf_militar) references militar (cpf),
     constraint registro_arma_fkey foreign key (registro_arma) references arma (numero_de_registro_pkey), -- verificar
