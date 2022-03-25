43363599621ebc3c73628ecb8daab417c0f88062
weslley batista <wbh@cin.ufpe.br>
2022-03-25 10:52:33 -0300

    fix variable type in tables

--- Tables/tables_weslley.sql
+++ Tables/tables_weslley.sql
@@ -1,11 +1,11 @@
 Create Table pessoa ( --entidade
     nome varchar2,
-    cpf number (11),
+    cpf varchar2,
     constraint pessoa_pkey primary key(cpf)
 );
 
 Create Table endereco_da_pessoa ( --composto
-    cpf_pessoa number(11),
+    cpf_pessoa varchar2,
     rua varchar2,
     numero number,
     complemento varchar2,
@@ -13,13 +13,13 @@ Create Table endereco_da_pessoa ( --composto
     constraint endereco_da_pessoa_fkey Foreign key (cpf_pessoa) References pessoa (cpf)
 );
 Create Table telefone_da_pessoa ( --multivalorado
-    cpf_pessoa number(11),
+    cpf_pessoa varchar2,
     numero_telefone number,
     constraint telefone_da_pessoa_fkey Foreign key (cpf_pessoa) References pessoa (cpf),
     constraint telefone_da_pessoa_const unique (numero_telefone)
 );
 Create Table prestador_de_serviço(
-    cpf number(11),
+    cpf varchar2),
     especialidade varchar2,
     constraint prestador_de_serviço_pkey primary key(cpf)
 );
