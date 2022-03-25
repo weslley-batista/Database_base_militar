937e945a776d286b6f1216c8de63da75550582f0
weslley batista <wbh@cin.ufpe.br>
2022-03-25 10:48:31 -0300

    estrutura do povoamento (Weslley)

---
+++ Tables/Povoamento_weslley.sql
@@ -0,0 +1,73 @@
+-- Inserindo pessoas
+
+INSERT INTO pessoa (nome,cpf) VALUES ();
+INSERT INTO pessoa (nome,cpf) VALUES ();
+INSERT INTO pessoa (nome,cpf) VALUES ();
+INSERT INTO pessoa (nome,cpf) VALUES ();
+INSERT INTO pessoa (nome,cpf) VALUES ();
+INSERT INTO pessoa (nome,cpf) VALUES ();
+INSERT INTO pessoa (nome,cpf) VALUES ();
+INSERT INTO pessoa (nome,cpf) VALUES ();
+INSERT INTO pessoa (nome,cpf) VALUES ();
+INSERT INTO pessoa (nome,cpf) VALUES ();
+INSERT INTO pessoa (nome,cpf) VALUES ();
+INSERT INTO pessoa (nome,cpf) VALUES ();
+INSERT INTO pessoa (nome,cpf) VALUES ();
+INSERT INTO pessoa (nome,cpf) VALUES ();
+INSERT INTO pessoa (nome,cpf) VALUES ();
+
+-- Inserindo endereco_da_pessoa
+
+INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ();
+INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ();
+INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ();
+INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ();
+INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ();
+INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ();
+INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ();
+INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ();
+INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ();
+INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ();
+INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ();
+INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ();
+INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ();
+INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ();
+INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ();
+INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ();
+
+-- Inserindo telefone_da_pessoa
+
+INSERT INTO telefone_da_pessoa (cpf_pessoa,numero_telefone) VALUES ();
+INSERT INTO telefone_da_pessoa (cpf_pessoa,numero_telefone) VALUES ();
+INSERT INTO telefone_da_pessoa (cpf_pessoa,numero_telefone) VALUES ();
+INSERT INTO telefone_da_pessoa (cpf_pessoa,numero_telefone) VALUES ();
+INSERT INTO telefone_da_pessoa (cpf_pessoa,numero_telefone) VALUES ();
+INSERT INTO telefone_da_pessoa (cpf_pessoa,numero_telefone) VALUES ();
+INSERT INTO telefone_da_pessoa (cpf_pessoa,numero_telefone) VALUES ();
+INSERT INTO telefone_da_pessoa (cpf_pessoa,numero_telefone) VALUES ();
+INSERT INTO telefone_da_pessoa (cpf_pessoa,numero_telefone) VALUES ();
+INSERT INTO telefone_da_pessoa (cpf_pessoa,numero_telefone) VALUES ();
+INSERT INTO telefone_da_pessoa (cpf_pessoa,numero_telefone) VALUES ();
+INSERT INTO telefone_da_pessoa (cpf_pessoa,numero_telefone) VALUES ();
+INSERT INTO telefone_da_pessoa (cpf_pessoa,numero_telefone) VALUES ();
+INSERT INTO telefone_da_pessoa (cpf_pessoa,numero_telefone) VALUES ();
+INSERT INTO telefone_da_pessoa (cpf_pessoa,numero_telefone) VALUES ();
+
+
+-- Inserindo prestador_de_serviço
+
+INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ();
+INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ();
+INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ();
+INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ();
+INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ();
+INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ();
+INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ();
+INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ();
+INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ();
+INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ();
+INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ();
+INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ();
+INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ();
+INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ();
+INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ();
