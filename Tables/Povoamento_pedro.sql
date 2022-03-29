c5453bee14cfa5e3602aaeeb309c16e7b5b73192
PedrovlBarbosa <55454762+PedrovlBarbosa@users.noreply.github.com>
2022-03-29 18:17:52 -0300

    update final

--- Tables/Povoamento_pedro.sql
+++ Tables/Povoamento_pedro.sql
@@ -1,34 +1,34 @@
 -- Inserindo arsenal
 
-INSERT INTO Arsenal (categoria_armazem, data_de_manutenção, capacidade_máxima, nome_base_militar) 
-    VALUES ('Fuzil','2022-05-10', 100, 'Arsenal_Fuzil 01');
-INSERT INTO Arsenal (categoria_armazem, data_de_manutenção, capacidade_máxima, nome_base_militar) 
-    VALUES ('Pistola','2023-01-01', 20, 'Arsenal_Pistola 01');
-INSERT INTO Arsenal (categoria_armazem, data_de_manutenção, capacidade_máxima, nome_base_militar) 
-    VALUES ('Pistola','2023-01-03', 20, 'Arsenal_Pistola 02');
-INSERT INTO Arsenal (categoria_armazem, data_de_manutenção, capacidade_máxima, nome_base_militar) 
-    VALUES ('Sub-Metralhadoras','2022-07-20', 80, 'Arsenal_Sub_Metralhadora 01');
+INSERT INTO Arsenal (id, categoria_armazem, data_de_manutenção, capacidade_máxima, nome_base_militar) 
+    VALUES (Id.NEXTVAL,'Fuzil',to_date('10/05/2022', 'dd/mm/yy'), 100, 'Arsenal_Fuzil 01');
+INSERT INTO Arsenal (id, categoria_armazem, data_de_manutenção, capacidade_máxima, nome_base_militar) 
+    VALUES (Id.NEXTVAL,'Pistola', to_date('01/01/2023', 'dd/mm/yy'), 20, 'Arsenal_Pistola 01');
+INSERT INTO Arsenal (id, categoria_armazem, data_de_manutenção, capacidade_máxima, nome_base_militar) 
+    VALUES (Id.NEXTVAL,'Pistola',to_date('03/01/2023', 'dd/mm/yy'), 20, 'Arsenal_Pistola 02');
+INSERT INTO Arsenal (id, categoria_armazem, data_de_manutenção, capacidade_máxima, nome_base_militar) 
+    VALUES (Id.NEXTVAL,'Sub-Metralhadoras',to_date('20/07/2022', 'dd/mm/yy'), 80, 'Arsenal_Sub_Metralhadora 01');
 
 -- inserindo fornecerArmamento
 
 INSERT INTO Fornecer_armamento (horario_e_data, cpf_militar, registro_arma, id_arsenal) 
-    VALUES ('2022-04-20 15:00:00','CPF MILITAR','01010202', 1);
+    VALUES (timestamp '2022-04-20 15:00:00','CPF MILITAR','01010202', 1); -- Necessário inserir o CPF MILITAR
 INSERT INTO Fornecer_armamento (horario_e_data, cpf_militar, registro_arma, id_arsenal) 
-    VALUES ('2022-04-20 15:00:00','CPF MILITAR','01013333', 2);
+    VALUES (timestamp '2022-04-20 15:00:00','CPF MILITAR','01013333', 2); -- Necessário inserir o CPF MILITAR
 INSERT INTO Fornecer_armamento (horario_e_data, cpf_militar, registro_arma, id_arsenal)
-    VALUES ('2022-04-25 07:30:00','CPF MILITAR','01014444', 3);
+    VALUES (timestamp '2022-04-25 07:30:00','CPF MILITAR','01014444', 3); -- Necessário inserir o CPF MILITAR
 INSERT INTO Fornecer_armamento (horario_e_data, cpf_militar, registro_arma, id_arsenal)
-    VALUES ('2022-05-01 09:30:00','CPF MILITAR','77777777', 4);
+    VALUES (timestamp '2022-05-01 09:30:00','CPF MILITAR','77777777', 4); -- Necessário inserir o CPF MILITAR
 
 
 -- inserindo arma
 
 INSERT INTO Arma (numero_de_registro, id_arsenal, calibre, fabricação, nome_do_armamento, horario_e_data) 
-    VALUES ('01010202', 1,'.556 Nato','Brasil','Carabina Taurus T4 A1 14,5','2022-04-20 15:00:00' );
+    VALUES ('01010202', 1,'.556 Nato','Brasil','Carabina Taurus T4 A1 14,5',timestamp '2022-04-20 15:00:00');
 INSERT INTO Arma (numero_de_registro, id_arsenal, calibre, fabricação, nome_do_armamento, horario_e_data) 
-    VALUES ('01013333', 2,'.380 Auto','Estados Unidos','Pistola Smith & Wesson Semiautomática M&P Shield EZ','2022-04-20 15:00:00' );
+    VALUES ('01013333', 2,'.380 Auto','Estados Unidos','Pistola Smith & Wesson Semiautomática',timestamp '2022-04-20 15:00:00');
 INSERT INTO Arma (numero_de_registro, id_arsenal, calibre, fabricação, nome_do_armamento, horario_e_data) 
-    VALUES ('01014444', 3,'.22LR','Áustria','Glock 44','2022-04-25 07:30:00');
+    VALUES ('01014444', 3,'.22LR','Áustria','Glock 44',timestamp '2022-04-25 07:30:00');
 INSERT INTO Arma (numero_de_registro, id_arsenal, calibre, fabricação, nome_do_armamento, horario_e_data) 
-    VALUES ('01014444', 4,'9mm','Alemanha','MP5A3','2022-05-01 09:30:00');
+    VALUES ('01014444', 4,'9mm','Alemanha','MP5A3',timestamp '2022-05-01 09:30:00');
 
--- Tables/table_pedro.sql
+++ Tables/table_pedro.sql
@@ -1,31 +1,38 @@
+DROP TABLE Arsenal;
+DROP TABLE Arma;
+DROP TABLE Fornecer_armamento;
+
+Create Sequence Id
+    INCREMENT BY 1 START WITH 1;
+    
 Create Table Arsenal ( -- entidade
-    Create Sequence id increment by 1 start with 1,
-    categoria_armazem varchar2 NOT NULL,
-    data_de_manutenção date NOT NULL,
-    capacidade_máxima int NOT NULL,
-    nome_base_militar varchar2 NOT NULL,
-    constraint id_pkey primary key (id),
-    constraint nome_base_militar_fkey foreign key (nome_base_militar) references Base_militar (nome)
+    Id NUMBER NOT NULL,
+    Categoria_armazem VARCHAR2 NOT NULL,
+    Data_de_manutenção DATE NOT NULL,
+    Capacidade_máxima NUMBER NOT NULL,
+    Nome_base_militar VARCHAR2 NOT NULL,
+    CONSTRAINT Id_pkey PRIMARY KEY (id),
+    CONSTRAINT Nome_base_militar_fkey FOREIGN KEY (Nome_base_militar) REFERENCES Base_militar (Nome)
 );
 
 Create Table Arma ( -- entidade
-    numero_de_registro bigint NOT NULL,
-    id_arsenal int NOT NULL, -- 
-    calibre varchar2 NOT NULL, -- .556
-    fabricação varchar2 NOT NULL,
-    nome_do_armamento varchar2 NOT NULL,
-    horario_e_data datetime2 NOT NULL,
-    constraint numero_de_registro_pkey primary key (numero_de_registro), -- verificar
-    constraint id_arsenal_fkey foreign key (id_arsenal) references Arsenal (id),
-    constraint horario_e_data_fkey foreign key (horario_e_data) references Fornecer_armamento (horario_e_data) -- verificar
+    Numero_de_registro BIGINT NOT NULL,
+    Id_arsenal NUMBER NOT NULL, -- 
+    Calibre VARCHAR2 NOT NULL, -- .556
+    Fabricação VARCHAR2 NOT NULL,
+    Nome_do_armamento VARCHAR2 NOT NULL,
+    Horario_e_data TIMESTAMP NOT NULL,
+    CONSTRAINT Numero_de_registro_pkey PRIMARY KEY (Numero_de_registro),
+    CONSTRAINT Id_arsenal_fkey FOREIGN KEY (Id_arsenal) REFERENCES Arsenal (Id),
+    CONSTRAINT Horario_e_data_fkey FOREIGN KEY (Horario_e_data) REFERENCES Fornecer_armamento (Horario_e_data)
 );
 
 Create Table Fornecer_armamento ( -- relacionamento triplo temporal
-    horario_e_data datetime2 NOT NULL,
-    cpf_militar varchar2 (14) NOT NULL, -- 000.000.000-00
-    registro_arma varchar2 NOT NULL,
-    id_arsenal int NOT NULL,
-    constraint cpf_militar_fkey foreign key (cpf_militar) references Militar (cpf),
-    constraint registro_arma_fkey foreign key (registro_arma) references Arma (numero_de_registro), -- verificar
-    constraint id_arsenal_fkey foreign key (id_arsenal) references Arsenal (id)
+    Horario_e_data TIMESTAMP NOT NULL,
+    Cpf_militar VARCHAR2 (14) NOT NULL, -- 000.000.000-00
+    Registro_arma VARCHAR2 NOT NULL,
+    Id_arsenal NUMBER NOT NULL,
+    CONSTRAINT Cpf_militar_fkey FOREIGN KEY (Cpf_militar) REFERENCES Militar (Cpf),
+    CONSTRAINT Registro_arma_fkey FOREIGN KEY (Registro_arma) REFERENCES Arma (Numero_de_registro),
+    CONSTRAINT Id_arsenal_fkey FOREIGN KEY (Id_arsenal) REFERENCES Arsenal (Id)
 );
