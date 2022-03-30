65c386093c792cae4b34490d3df37bda8c6b3cf3
weslley-batista <wbh@cin.ufpe.br>
2022-03-29 23:36:21 -0300

    Ajuste povoamento_weslley

--- Tables/Povoamento_weslley.sql
+++ Tables/Povoamento_weslley.sql
@@ -10,11 +10,22 @@ INSERT INTO pessoa (nome,cpf) VALUES ('Davi','413.372.547-62');
 INSERT INTO pessoa (nome,cpf) VALUES ('Laura','606.862.377-73');
 INSERT INTO pessoa (nome,cpf) VALUES ('Gabriel','644.718.711-88');
 INSERT INTO pessoa (nome,cpf) VALUES ('Valentina','415.332.550-32');
+--Prestadores de serviço acima ⬆
 INSERT INTO pessoa (nome,cpf) VALUES ('Bernardo','266.873.194-18');
 INSERT INTO pessoa (nome,cpf) VALUES ('Samuel','060.718.346-27');
 INSERT INTO pessoa (nome,cpf) VALUES ('João Miguel','730.774.348-57');
 INSERT INTO pessoa (nome,cpf) VALUES ('Heloísa','407.436.138-80');
 INSERT INTO pessoa (nome,cpf) VALUES ('Isabela','923.676.914-44');
+INSERT INTO pessoa (nome,cpf) VALUES ('Raquel Maranhão','439.509.231-44');
+INSERT INTO pessoa (nome,cpf) VALUES ('Edir Carneiro','050.365.042-08');
+INSERT INTO pessoa (nome,cpf) VALUES ('Lola Lisboa','028.271.834-63');
+INSERT INTO pessoa (nome,cpf) VALUES ('Artyom Carmona','219.548.474-83');
+INSERT INTO pessoa (nome,cpf) VALUES ('Carla Guerra','575.283.272-12');
+INSERT INTO pessoa (nome,cpf) VALUES ('Evandro Malta','335.206.930-11');
+INSERT INTO pessoa (nome,cpf) VALUES ('Zoé Caldeira','247.298.232-14');
+INSERT INTO pessoa (nome,cpf) VALUES ('Cláudio Brites','877.119.269-79');
+INSERT INTO pessoa (nome,cpf) VALUES ('Liany Veloso','147.114.791-82');
+INSERT INTO pessoa (nome,cpf) VALUES ('Ary Veloso','243.832.546-16');
 
 -- Inserindo endereco_da_pessoa
 
@@ -33,6 +44,16 @@ INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUE
 INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ('730.774.348-57','Avenida Luiz Avelino Pereira', 136,'Tabuleiro do Martins','57081-131');
 INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ('407.436.138-80', 'Avenida das Gaivotas', 270, 'Loteamento Parque dos Pássaros', '15093-636');
 INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ('923.676.914-44','Rua Maria de Lourdes de Melo', 769, 'Vida Nova','59147-840');
+INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ('439.509.231-44','Rua Armando Torres Monteiro', 569, 'Iputinga','50690-200');
+INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ('050.365.042-08','Rua Um', 998, 'Altos do Coxipó','78088-465');
+INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ('028.271.834-63','Rua Leste', 322, 'Novo Horizonte','76810-210');
+INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ('219.548.474-83','Rua C', 657, 'Reis Veloso','64204-080');
+INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ('575.283.272-12','Quadra QS 514 Conjunto 1', 125, 'Pitanguinha','57052-220');
+INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ('335.206.930-11','Alameda Oitava', 486, 'Coqueiro','66670-790');
+INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ('247.298.232-14','Travessa Três', 789, 'Bela Vista','68374-716');
+INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ('877.119.269-79','Rua Primeiro de Maio', 752, 'José Pinheiro','58407-410');
+INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ('147.114.791-82','Quadra QE 5 Conjunto E', 421, 'Guará I','71020-055');
+INSERT INTO endereco_da_pessoa (cpf_pessoa, rua, numero, complemento, cep) VALUES ('243.832.546-16','Rua Tenente Guimarães', 934, 'Liberdade','69309-109');
 
 -- Inserindo telefone_da_pessoa
 
@@ -54,13 +75,13 @@ INSERT INTO telefone_da_pessoa (cpf_pessoa,numero_telefone) VALUES ('923.676.914
 
 -- Inserindo prestador_de_serviço
 
-INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ('439.509.231-44', 'Coaching');
-INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ('050.365.042-08', 'Instruções de etiqueta');    
-INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ('028.271.834-63', 'Limpeza de bolsa de gordura');
-INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ('219.548.474-83', 'Entrega de alimentos');
-INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ('575.283.272-12', 'Maneabilidade de produtos quimicos');
-INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ('335.206.930-11', 'Exposição de arte');
-INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ('247.298.232-14', 'Cortes de cabelos');
-INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ('877.119.269-79', 'Renovação de vacinas');
-INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ('147.114.791-82', 'Coaching');
-INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ('243.832.546-16', 'Tecnologia militar');
+INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ('474.020.744-39', 'Coaching');
+INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ('84.627.718-54', 'Instruções de etiqueta');    
+INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ('632.481.319-34', 'Limpeza de bolsa de gordura');
+INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ('977.827.224-70', 'Entrega de alimentos');
+INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ('134.177.826-60', 'Maneabilidade de produtos quimicos');
+INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ('308.175.745-22', 'Exposição de arte');
+INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ('413.372.547-62', 'Cortes de cabelos');
+INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ('606.862.377-73', 'Renovação de vacinas');
+INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ('644.718.711-88', 'Coaching');
+INSERT INTO prestador_de_serviço (cpf, especialidade) VALUES ('415.332.550-32', 'Tecnologia militar');
