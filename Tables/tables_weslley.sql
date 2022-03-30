e3a1b55e2ddf7f820680b08bc21b971238c75b8e
weslley-batista <wbh@cin.ufpe.br>
2022-03-29 22:59:32 -0300

    Reformatando variaveis e palavras chaves

--- Tables/tables_weslley.sql
+++ Tables/tables_weslley.sql
@@ -1,25 +1,25 @@
-CREATE TABLE pessoa ( --entidade
-    nome varchar2 (255),
-    cpf varchar2 (14),
-    constraint pessoa_pkey primary key(cpf)
+CREATE TABLE Pessoa ( --entidade
+    Nome VARCHAR2 (255),
+    CPF VARCHAR2 (14),
+    CONSTRAINT Pessoa_pkey PRIMARY KEY(CPF)
 );
 
-CREATE TABLE endereco_da_pessoa ( --composto
-    cpf_pessoa varchar2 (14),
-    rua varchar2 (255),
-    numero number (38),
-    complemento varchar2 (255),
-    cep varchar2 (14),
-    constraint endereco_da_pessoa_fkey Foreign key (cpf_pessoa) References pessoa (cpf)
+CREATE TABLE Endereco_da_pessoa ( --composto
+    Cpf_pessoa VARCHAR2 (14),
+    Rua VARCHAR2 (255),
+    Numero NUMBER (38),
+    Complemento VARCHAR2 (255),
+    CEP VARCHAR2 (14),
+    CONSTRAINT Endereco_da_pessoa_fkey FOREIGN KEY (Cpf_pessoa) REFERENCES Pessoa (CPF)
 );
-CREATE TABLE telefone_da_pessoa ( --multivalorado
-    cpf_pessoa varchar2 (14),
-    numero_telefone varchar2 (15),
-    constraint telefone_da_pessoa_fkey Foreign key (cpf_pessoa) References pessoa (cpf),
-    constraint telefone_da_pessoa_const unique (numero_telefone)
+CREATE TABLE Telefone_da_pessoa ( --multivalorado
+    Cpf_pessoa VARCHAR2 (14),
+    Numero_telefone VARCHAR2 (15),
+    CONSTRAINT Telefone_da_pessoa_fkey FOREIGN KEY (Cpf_pessoa) REFERENCES Pessoa (CPF),
+    CONSTRAINT Telefone_da_pessoa_const unique (Numero_telefone)
 );
-CREATE TABLE prestador_de_serviço(
-    cpf varchar2 (14),
-    especialidade varchar2 (255),
-    constraint prestador_de_serviço_pkey primary key(cpf)
+CREATE TABLE Prestador_de_serviço(
+    CPF VARCHAR2 (14),
+    Especialidade VARCHAR2 (255),
+    CONSTRAINT Prestador_de_serviço_pkey PRIMARY KEY(CPF)
 );
