a40a2dbba7674b305501b7ee6673c9b5dc94e0d4
José Wilson Cavalcante Ferreira Junior <jwcfj@cin.ufpe.br>
2022-03-31 12:20:40 -0300

    Inseri as alterações dos outros commits de povoamento nesse arquivo principal

--- povoamento_BaseMilitar.sql
+++ povoamento_BaseMilitar.sql
@@ -41,6 +41,8 @@ INSERT INTO militar(cpf, patente) VALUES ('413.372.547-62', 1);
 INSERT INTO militar(cpf, patente) VALUES ('606.862.377-73', 5);
 INSERT INTO militar(cpf, patente) VALUES ('644.718.711-88', 2);
 INSERT INTO militar(cpf, patente) VALUES ('415.332.550-32', 1);
+INSERT INTO militar(cpf, patente) VALUES ('219.548.474-83', 5);
+INSERT INTO militar(cpf, patente) VALUES ('335.206.930-11', 5);
 
 -- Inserindo endereco_da_pessoa
 
@@ -197,42 +199,44 @@ INSERT INTO endereco_base(nome_base, cep, rua, numero, complemento) VALUES('BIM'
 INSERT INTO endereco_base(nome_base, cep, rua, numero, complemento) VALUES('BC', 50930000, 'BR-101', NULL, 'Km 73');
 
 -- Inserindo Militar Comandado
-INSERT INTO Militar_comandado(militar_comandante_cpf, militar_comandado_cpf, quadrante_coordenadas, numero_agrupamento) VALUES ('00000000001', '00000000002','5444',  1);
-INSERT INTO Militar_comandado(militar_comandante_cpf, militar_comandado_cpf, quadrante_coordenadas, numero_agrupamento) VALUES ('00000000001', '00000000003','5444',  1);
-INSERT INTO Militar_comandado(militar_comandante_cpf, militar_comandado_cpf, quadrante_coordenadas, numero_agrupamento) VALUES ('00000000001', '00000000004','5444',  1);
-INSERT INTO Militar_comandado(militar_comandante_cpf, militar_comandado_cpf, quadrante_coordenadas, numero_agrupamento) VALUES ('00000000001', '00000000005','5444',  1);
-INSERT INTO Militar_comandado(militar_comandante_cpf, militar_comandado_cpf, quadrante_coordenadas, numero_agrupamento) VALUES ('00000000006', '00000000007','5443',  2);
-INSERT INTO Militar_comandado(militar_comandante_cpf, militar_comandado_cpf, quadrante_coordenadas, numero_agrupamento) VALUES ('00000000006', '00000000008','5443',  2);
-INSERT INTO Militar_comandado(militar_comandante_cpf, militar_comandado_cpf, quadrante_coordenadas, numero_agrupamento) VALUES ('00000000006', '00000000009','5443',  2);
-INSERT INTO Militar_comandado(militar_comandante_cpf, militar_comandado_cpf, quadrante_coordenadas, numero_agrupamento) VALUES ('00000000006', '00000000010','5443',  2);
-INSERT INTO Militar_comandado(militar_comandante_cpf, militar_comandado_cpf, quadrante_coordenadas, numero_agrupamento) VALUES ('00000000006', '00000000011','5443',  2);
-INSERT INTO Militar_comandado(militar_comandante_cpf, militar_comandado_cpf, quadrante_coordenadas, numero_agrupamento) VALUES ('00000000006', '00000000012','5443',  2);
+INSERT INTO Militar_comandado(militar_comandante_cpf, militar_comandado_cpf, quadrante_coordenadas, numero_agrupamento) VALUES ('219.548.474-83', '384.627.718-54','50° 0’36.30″N, 110° 6’46.82″W',  1);
+INSERT INTO Militar_comandado(militar_comandante_cpf, militar_comandado_cpf, quadrante_coordenadas, numero_agrupamento) VALUES ('219.548.474-83', '632.481.319-34','50° 0’36.30″N, 110° 6’46.82″W',  1);
+INSERT INTO Militar_comandado(militar_comandante_cpf, militar_comandado_cpf, quadrante_coordenadas, numero_agrupamento) VALUES ('219.548.474-83', '977.827.224-70','50° 0’36.30″N, 110° 6’46.82″W',  1);
+INSERT INTO Militar_comandado(militar_comandante_cpf, militar_comandado_cpf, quadrante_coordenadas, numero_agrupamento) VALUES ('219.548.474-83', '134.177.826-60','50° 0’36.30″N, 110° 6’46.82″W',  1);
+INSERT INTO Militar_comandado(militar_comandante_cpf, militar_comandado_cpf, quadrante_coordenadas, numero_agrupamento) VALUES ('219.548.474-83', '474.020.744-39','50° 0’36.30″N, 110° 6’46.82″W',  1);
+INSERT INTO Militar_comandado(militar_comandante_cpf, militar_comandado_cpf, quadrante_coordenadas, numero_agrupamento) VALUES ('335.206.930-11', '413.372.547-62','40°27’28.56″N, 93°23’34.42″E',  2);
+INSERT INTO Militar_comandado(militar_comandante_cpf, militar_comandado_cpf, quadrante_coordenadas, numero_agrupamento) VALUES ('335.206.930-11', '606.862.377-73','40°27’28.56″N, 93°23’34.42″E',  2);
+INSERT INTO Militar_comandado(militar_comandante_cpf, militar_comandado_cpf, quadrante_coordenadas, numero_agrupamento) VALUES ('335.206.930-11', '644.718.711-88','40°27’28.56″N, 93°23’34.42″E',  2);
+INSERT INTO Militar_comandado(militar_comandante_cpf, militar_comandado_cpf, quadrante_coordenadas, numero_agrupamento) VALUES ('335.206.930-11', '415.332.550-32','40°27’28.56″N, 93°23’34.42″E',  2);
+INSERT INTO Militar_comandado(militar_comandante_cpf, militar_comandado_cpf, quadrante_coordenadas, numero_agrupamento) VALUES ('335.206.930-11', '308.175.745-22','40°27’28.56″N, 93°23’34.42″E',  2);
+
 
 -- Inserindo Visistante
 
-INSERT INTO Visitante(nome , cpf_militar) VALUES ('luffy', 'cpf 1');
-INSERT INTO Visitante(nome , cpf_militar) VALUES ('zoro', 'cpf 2');
-INSERT INTO Visitante(nome , cpf_militar) VALUES ('sanji', 'cpf 3');
-INSERT INTO Visitante(nome , cpf_militar) VALUES ('usopp', 'cpf 4');
-INSERT INTO Visitante(nome , cpf_militar) VALUES ('nami', 'cpf 5');
-INSERT INTO Visitante(nome , cpf_militar) VALUES ('chopper', 'cpf 6');
-INSERT INTO Visitante(nome , cpf_militar) VALUES ('robin', 'cpf 7');
-INSERT INTO Visitante(nome , cpf_militar) VALUES ('franky', 'cpf 8');
-INSERT INTO Visitante(nome , cpf_militar) VALUES ('brook', 'cpf 9');
-INSERT INTO Visitante(nome , cpf_militar) VALUES ('robin', 'cpf 10');
+INSERT INTO Visitante(nome , cpf_militar) VALUES ('luffy', '474.020.744-39');
+INSERT INTO Visitante(nome , cpf_militar) VALUES ('zoro', '219.548.474-83');
+INSERT INTO Visitante(nome , cpf_militar) VALUES ('sanji', '632.481.319-34');
+INSERT INTO Visitante(nome , cpf_militar) VALUES ('usopp', '977.827.224-704');
+INSERT INTO Visitante(nome , cpf_militar) VALUES ('nami', '134.177.826-60');
+INSERT INTO Visitante(nome , cpf_militar) VALUES ('chopper', '413.372.547-62');
+INSERT INTO Visitante(nome , cpf_militar) VALUES ('robin', '606.862.377-73');
+INSERT INTO Visitante(nome , cpf_militar) VALUES ('franky', '335.206.930-11');
+INSERT INTO Visitante(nome , cpf_militar) VALUES ('brook', '644.718.711-88');
+INSERT INTO Visitante(nome , cpf_militar) VALUES ('jinbe', '415.332.550-32');
 
 -- Inserindo Comunicação entre bases
 
-INSERT INTO Comunicação_entre_bases(base_inicia_cominicação , base_recebe_comunicação) VALUES ('base 1', 'base 2');
-INSERT INTO Comunicação_entre_bases(base_inicia_cominicação , base_recebe_comunicação) VALUES ('base 2', 'base 3');
-INSERT INTO Comunicação_entre_bases(base_inicia_cominicação , base_recebe_comunicação) VALUES ('base 3', 'base 4');
-INSERT INTO Comunicação_entre_bases(base_inicia_cominicação , base_recebe_comunicação) VALUES ('base 4', 'base 5');
-INSERT INTO Comunicação_entre_bases(base_inicia_cominicação , base_recebe_comunicação) VALUES ('base 5', 'base 6');
-INSERT INTO Comunicação_entre_bases(base_inicia_cominicação , base_recebe_comunicação) VALUES ('base 6', 'base 7');
-INSERT INTO Comunicação_entre_bases(base_inicia_cominicação , base_recebe_comunicação) VALUES ('base 7', 'base 8');
-INSERT INTO Comunicação_entre_bases(base_inicia_cominicação , base_recebe_comunicação) VALUES ('base 8', 'base 9');
-INSERT INTO Comunicação_entre_bases(base_inicia_cominicação , base_recebe_comunicação) VALUES ('base 9', 'base 10');
-INSERT INTO Comunicação_entre_bases(base_inicia_cominicação , base_recebe_comunicação) VALUES ('base 10', 'base 11');
+INSERT INTO Comunicação_entre_bases(base_inicia_cominicação , base_recebe_comunicação) VALUES ('BAAN', 'BACO');
+INSERT INTO Comunicação_entre_bases(base_inicia_cominicação , base_recebe_comunicação) VALUES ('BACO', 'BAFZ');
+INSERT INTO Comunicação_entre_bases(base_inicia_cominicação , base_recebe_comunicação) VALUES ('BAAN', 'BAFZ');
+INSERT INTO Comunicação_entre_bases(base_inicia_cominicação , base_recebe_comunicação) VALUES ('BAFZ', 'BANT');
+INSERT INTO Comunicação_entre_bases(base_inicia_cominicação , base_recebe_comunicação) VALUES ('BAAN', 'BANT');
+INSERT INTO Comunicação_entre_bases(base_inicia_cominicação , base_recebe_comunicação) VALUES ('BARF', 'BACO');
+INSERT INTO Comunicação_entre_bases(base_inicia_cominicação , base_recebe_comunicação) VALUES ('BAST', 'BAAF');
+INSERT INTO Comunicação_entre_bases(base_inicia_cominicação , base_recebe_comunicação) VALUES ('BAAF', 'BAFZ');
+INSERT INTO Comunicação_entre_bases(base_inicia_cominicação , base_recebe_comunicação) VALUES ('BAFZ', 'BACO');
+INSERT INTO Comunicação_entre_bases(base_inicia_cominicação , base_recebe_comunicação) VALUES ('BAAN', 'BAST');
+
 
 -- Inserindo arsenal
 
