0ae830dcba39262359addf10a73a94efdaf55183
weslley-batista <wbh@cin.ufpe.br>
2022-03-31 12:14:45 -0300

    renomeando tarefas

--- table_BaseMilitar.sql
+++ table_BaseMilitar.sql
@@ -7,8 +7,8 @@ CREATE TABLE Pessoa ( --entidade
 Create Table militar (
     cpf VARCHAR2(14),
     patente NUMBER NOT NULL,
-    CONSTRAINT militar_pk PRIMARY KEY (cpf),
-    CONSTRAINT militar_cpf FOREIGN KEY (cpf) REFERENCES pessoa (cpf)
+    CONSTRAINT militar_pkey PRIMARY KEY (cpf),
+    CONSTRAINT militar_fkey FOREIGN KEY (cpf) REFERENCES pessoa (CPF)
 );
 
 CREATE TABLE Endereco_da_pessoa ( --composto
@@ -31,7 +31,7 @@ CREATE TABLE Prestador_de_serviço(
     CPF VARCHAR2 (14),
     Especialidade VARCHAR2 (255),
     CONSTRAINT Prestador_de_serviço_pkey PRIMARY KEY(CPF),
-    CONSTRAINT Prestador_de_serviço_Fk FOREIGN KEY (CPF) REFERENCES Pessoa (CPF)
+    CONSTRAINT Prestador_de_serviço_fkey FOREIGN KEY (CPF) REFERENCES Pessoa (CPF)
 );
 
 Create Sequence id_servico_seq
@@ -48,37 +48,37 @@ Create Table executa_servico (
     Prestador_de_servico NUMBER(14),
     Servico NUMBER,
     Cpf_militar varchar2(14)
-    CONSTRAINT prestador_de_servico_fkey FOREIGN KEY (prestador_de_servico) REFERENCES Prestador_de_serviço(cpf),
-    CONSTRAINT servico_fkey FOREIGN key (servico) REFERENCES servico (Id_servico),
-    CONSTRAINT cpf_militar_fkey FOREIGN KEY (cpf_militar) REFERENCES militar (cpf)
+    CONSTRAINT executa_servico_fkey FOREIGN KEY (prestador_de_servico) REFERENCES Prestador_de_serviço(cpf),
+    CONSTRAINT executa_servico_fkey FOREIGN key (servico) REFERENCES servico (Id_servico),
+    CONSTRAINT executa_servico_fkey FOREIGN KEY (cpf_militar) REFERENCES militar (cpf)
 );
 
 Create Table Quadrante ( 
     Coordenadas VARCHAR2, 
     Terreno VARCHAR2,    
-    CONSTRAINT coordenadas_pkey PRIMARY KEY(coordenadas)
+    CONSTRAINT Quadrante_pkey PRIMARY KEY(coordenadas)
     
 );
 
 Create Table condecoracoes (
     cpf VARCHAR2(14),
     condecoracao VARCHAR2(30),
-    CONSTRAINT condecoracoes_pk PRIMARY KEY (cpf),
-    CONSTRAINT condecoracoes_cpf FOREIGN KEY (cpf) REFERENCES militar(cpf)
+    CONSTRAINT condecoracoes_pkey PRIMARY KEY (cpf),
+    CONSTRAINT condecoracoes_fkey FOREIGN KEY (cpf) REFERENCES militar(cpf)
 );
 
 Create Table base_militar (
     nome VARCHAR2(30),
     especialidade VARCHAR2(30) NOT NULL,
-    CONSTRAINT base_pk PRIMARY KEY (nome)
+    CONSTRAINT base_militar_pkey PRIMARY KEY (nome)
 );
 
 Create Table telefone_base (
     nome_base VARCHAR2(30),
     ddd NUMBER NOT NULL,
     numero NUMBER NOT NULL,
-    CONSTRAINT telefone_pk PRIMARY KEY (nome_base),
-    CONSTRAINT telefone_nome FOREIGN KEY (nome_base) REFERENCES base_militar(nome)
+    CONSTRAINT telefone_base_pkey PRIMARY KEY (nome_base),
+    CONSTRAINT telefone_base_fkey FOREIGN KEY (nome_base) REFERENCES base_militar(nome)
 );
 
 Create Table endereco_base (
@@ -87,8 +87,8 @@ Create Table endereco_base (
     rua VARCHAR2(30) NOT NULL,
     numero NUMBER,
     complemento VARCHAR2(10),
-    CONSTRAINT endereco_pk PRIMARY KEY (nome_base),
-    CONSTRAINT endereco_nome FOREIGN KEY (nome_base) REFERENCES base_militar(nome)
+    CONSTRAINT endereco_base_pkey PRIMARY KEY (nome_base),
+    CONSTRAINT endereco_base_fkey FOREIGN KEY (nome_base) REFERENCES base_militar(nome)
 );
 
 CREATE TABLE Militar_comandado(
@@ -96,23 +96,23 @@ CREATE TABLE Militar_comandado(
     militar_comandado_cpf VARCHAR2(14) NOT NULL,
     quadrante_coordenadas VARCHAR2(11),
     numero_agrupamento INTEGER NOT NULL,
-    CONSTRAINT militar_comandado_pk PRIMARY KEY (militar_comandante_cpf,militar_comandado_cpf),
-    CONSTRAINT militar_comandado_fk FOREIGN KEY (militar_comandado_fk, militar_comandante_cpf) REFERENCES militar (cpf),
-    CONSTRAINT militar_comandado_fk FOREIGN KEY (quadrante_coordenadas) REFERENCES Quadrante(coordenadas)
+    CONSTRAINT militar_comandado_pkey PRIMARY KEY (militar_comandante_cpf,militar_comandado_cpf),
+    CONSTRAINT militar_comandado_fkey FOREIGN KEY (militar_comandado_fk, militar_comandante_cpf) REFERENCES militar (cpf),
+    CONSTRAINT militar_comandado_fkey FOREIGN KEY (quadrante_coordenadas) REFERENCES Quadrante(coordenadas)
 );
 
 CREATE TABLE Visitante(
     nome VARCHAR2(100) NOT NULL,
     cpf_militar VARCHAR2(11) NOT NULL,
-    CONSTRAINT Visitante_pk PRIMARY KEY (nome,cpf_militar),
-    CONSTRAINT visitante_FK FOREIGN KEY (cpf_militar) REFERENCES militar (CPF)
+    CONSTRAINT Visitante_pkey PRIMARY KEY (nome,cpf_militar),
+    CONSTRAINT visitante_fkey FOREIGN KEY (cpf_militar) REFERENCES militar (CPF)
 );
 
 CREATE TABLE Comunicação_entre_bases(
     base_inicia_cominicação VARCHAR2(100) NOT NULL,
     base_recebe_comunicação VARCHAR2(100) NOT NULL,
-    CONSTRAINT Comunicação_entre_bases_fk1 FOREIGN KEY (base_inicia_cominicação) REFERENCES Base_militar(nome),
-    CONSTRAINT Comunicação_entre_bases_fk2 FOREIGN KEY (base_recebe_comunicação) REFERENCES Base_militar(nome)
+    CONSTRAINT Comunicação_entre_bases_fkey FOREIGN KEY (base_inicia_cominicação) REFERENCES Base_militar(nome),
+    CONSTRAINT Comunicação_entre_bases_fkey FOREIGN KEY (base_recebe_comunicação) REFERENCES Base_militar(nome)
 );
 
 CREATE SEQUENCE Id
@@ -124,8 +124,8 @@ CREATE TABLE Arsenal ( -- entidade
     Data_de_manutencao DATE NOT NULL,
     Capacidade_maxima NUMBER NOT NULL,
     Nome_base_militar VARCHAR2 NOT NULL,
-    CONSTRAINT Id_pkey PRIMARY KEY (Id),
-    CONSTRAINT Nome_base_militar_fkey FOREIGN KEY (Nome_base_militar) REFERENCES Base_militar (Nome)
+    CONSTRAINT Arsenal_pkey PRIMARY KEY (Id),
+    CONSTRAINT Arsenal_fkey FOREIGN KEY (Nome_base_militar) REFERENCES Base_militar (Nome)
 );
 
 CREATE TABLE Arma ( -- entidade
@@ -134,8 +134,8 @@ CREATE TABLE Arma ( -- entidade
     Calibre VARCHAR2 NOT NULL, -- .556
     Fabricacao VARCHAR2 NOT NULL,
     Nome_do_armamento VARCHAR2 NOT NULL,
-    CONSTRAINT Numero_de_registro_pkey PRIMARY KEY (Numero_de_registro),
-    CONSTRAINT Id_arsenal_fkey FOREIGN KEY (Id_arsenal) REFERENCES Arsenal (Id),
+    CONSTRAINT Arma_pkey PRIMARY KEY (Numero_de_registro),
+    CONSTRAINT Arma_fkey FOREIGN KEY (Id_arsenal) REFERENCES Arsenal (Id),
 );
 
 CREATE TABLE Fornecer_armamento ( -- relacionamento triplo temporal
@@ -143,9 +143,9 @@ CREATE TABLE Fornecer_armamento ( -- relacionamento triplo temporal
     Cpf_militar VARCHAR2 (14) NOT NULL, -- 000.000.000-00
     Registro_arma VARCHAR2 NOT NULL,
     Id_arsenal NUMBER NOT NULL,
-    CONSTRAINT Cpf_militar_fkey FOREIGN KEY (Cpf_militar) REFERENCES Militar (Cpf),
-    CONSTRAINT Registro_arma_fkey FOREIGN KEY (Registro_arma) REFERENCES Arma (Numero_de_registro),
-    CONSTRAINT Id_arsenal_fkey FOREIGN KEY (Id_arsenal) REFERENCES Arsenal (Id)
+    CONSTRAINT Fornecer_armamento_fkey FOREIGN KEY (Cpf_militar) REFERENCES Militar (Cpf),
+    CONSTRAINT Fornecer_armamento_fkey FOREIGN KEY (Registro_arma) REFERENCES Arma (Numero_de_registro),
+    CONSTRAINT Fornecer_armamento_fkey FOREIGN KEY (Id_arsenal) REFERENCES Arsenal (Id)
 );
 
 
