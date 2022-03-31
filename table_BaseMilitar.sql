578592c77c1cb34309e8f19b9ec5937aa3762f4d
José Wilson Cavalcante Ferreira Junior <jwcfj@cin.ufpe.br>
2022-03-31 13:25:53 -0300

    Corrigi um erro de digitação

--- table_BaseMilitar.sql
+++ table_BaseMilitar.sql
@@ -112,7 +112,7 @@ CREATE TABLE Visitante(
 );
 
 
-CREATE TABLE Comunicação_entre_bases(
+CREATE TABLE Comunicacao_entre_bases(
     base_inicia_cominicação VARCHAR2(100) NOT NULL,
     base_recebe_comunicação VARCHAR2(100) NOT NULL,
     CONSTRAINT Comunicação_entre_bases_fk1 FOREIGN KEY (base_inicia_cominicação) REFERENCES Base_militar(nome),
