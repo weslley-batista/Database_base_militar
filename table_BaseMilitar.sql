05da6679c02e969ed323c7e1c70ff0cb09e0cd61
Julio Vinicius <55923294+jvgsantos@users.noreply.github.com>
2022-03-30 23:54:17 -0300

    correção prestador

--- table_BaseMilitar.sql
+++ table_BaseMilitar.sql
@@ -47,7 +47,7 @@ Create Table executa_servico (
     Prestador_de_servico NUMBER(11), 
     Servico NUMBER,
     Cpf_militar varchar2(14)
-    CONSTRAINT prestador_de_servico_fkey FOREIGN KEY (prestador_de_servico) REFERENCES prestador_de_serviço (cpf),
+    CONSTRAINT prestador_de_servico_fkey FOREIGN KEY (prestador_de_servico) REFERENCES Prestador_de_serviço(cpf),
     CONSTRAINT servico_fkey FOREIGN key (servico) REFERENCES servico (id),
     CONSTRAINT cpf_militar_fkey FOREIGN KEY (cpf_militar) REFERENCES militar (cpf)
 );
