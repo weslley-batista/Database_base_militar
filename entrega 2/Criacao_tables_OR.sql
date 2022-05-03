2ed62704f543cdb49238673c0b57d0f5631b28b5
PedrovlBarbosa <55454762+PedrovlBarbosa@users.noreply.github.com>
2022-05-03 00:13:21 -0300

    criando tabela base

--- entrega 2/Criacao_tables_OR.sql
+++ entrega 2/Criacao_tables_OR.sql
@@ -18,3 +18,18 @@ CREATE TABLE tb_militar OF tp_militar(
     Patente NOT NULL,
     lista_telefone tp_nt_telefone
 ) NESTED TABLE lista_telefone STORE AS tb_lista_telefone;
+/
+CREATE TABLE tp_telefone_base OF tp_telefone_base(
+    Numero tp_arr_telefone_base NOT NULL
+);
+/
+CREATE TABLE tp_endereco_base OF tp_endereco_base(
+    Nome NOT NULL,
+    CEP NOT NULL
+);
+/
+CREATE TABLE tp_base OF tp_base(
+    Nome PRIMARY KEY,
+    Especialidade NOT NULL,
+    lista_telefone_base tp_nt_telefone_base
+) NESTED TABLE lista_telefone_base STORE AS tb_lista_telefone_base;
